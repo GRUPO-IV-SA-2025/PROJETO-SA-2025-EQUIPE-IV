e354b6e992df5bc45ae62ebadbc9cdd13f0f87b9
Allyson Felippo <allysonfelippo35@gmail.com>
2025-03-03 20:20:57 -0300

    Tiítulo sobre projeto e infos + img ilustrativa com btn newuser

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,19 @@
 import './Body.css'
 
-function Body(){
+function Body() {
     return (
         <div className="container-body">
-    
-        </div>
+            <div className='titulo-texto-imagem'>
+                <div className='container-texto-btn'>
+                    <h2>Sistema de gerenciamento e controle de estoque para sua empresa</h2>
+                    <p>
+                        Com o sistema de gereanciamento e controle de estoque <span>Estoque+</span>, você organiza e movimenta com eficiência o estoque de sua empresa. Simplifique sua gestão agora!
+                    </p>
+                    <button className='btn-new-user-style style-btns'>Criar Conta</button>
+                </div>
+                <img className='img-style' src="./images/warehouse-worker.avif" alt="" />
+            </div>
+        </div >
     )
 }
 
