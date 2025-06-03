625437223956749b1c2438bbdc10956df264a87e
luccaalvess <lucca-alves@hotmail.com>
2025-06-02 21:49:04 -0300

    adição erro visual campos cadastro/login

--- src/telas/Cadastro/Cadastro.jsx
+++ src/telas/Cadastro/Cadastro.jsx
@@ -2,8 +2,72 @@ import { Box, Button, Stack, TextField } from '@mui/material';
 import './Cadastro.css';
 import LogoProjeto from '../../components/LogoProjeto/LogoProjeto';
 import imgWorker from '/images/Checking boxes-amico.svg';
+import { useState } from 'react';
 
 function TelaCadastro() {
+
+
+    const [formData, setFormData] = useState({
+        empresa: '',
+        nome: '',
+        sobrenome: '',
+        cnpj: '',
+        contato: '',
+        email: '',
+        senha: ''
+    });
+
+    const [errors, setErrors] = useState({
+        empresa: false,
+        nome: false,
+        sobrenome: false,
+        cnpj: false,
+        contato: false,
+        email: false,
+        senha: false
+    });
+
+    const handleChange = (e) => {
+        // setFormData({ ...formData, [e.target.name]: e.target.value });
+
+        const { name, value } = e.target;
+        setFormData(prev => ({ ...prev, [name]: value }));
+        if (value.trim() !== '') {
+            setErrors(prev => ({ ...prev, [name]: false }));
+        }
+    };
+
+
+    // verifica se os campos estao preenchidos
+    const handleSubmit = () => {
+        // const camposVazios = Object.entries(formData).filter(([chave, valor]) => valor.trim() === '');
+
+        // if (camposVazios.length > 0) {
+        //     alert(`Preencha todos os campos. Campos faltando: ${camposVazios.map(([chave]) => chave).join(', ')}`);
+        //     return;
+        // }
+
+        const novosErros = {};
+        let hasError = false;
+
+        for (const campo in formData) {
+            if (formData[campo].trim() === '') {
+                novosErros[campo] = true;
+                hasError = true;
+            } else {
+                novosErros[campo] = false;
+            }
+        }
+
+        setErrors(novosErros);
+
+        if (hasError) {
+            return;
+        }
+
+        alert('Cadastro realizado com sucesso!');
+        // Aqui pode chamar uma função para enviar os dados para o backend
+    };
 
     return (
         <Box sx={{ width: '100vw', height: '100vh', gridTemplateColumns: '1fr 1fr', display: 'flex' }}>
@@ -11,14 +75,21 @@ function TelaCadastro() {
                 <Stack spacing={3} sx={{ width: '50%', alignItems: 'center' }}>
                     <LogoProjeto />
                     <Stack spacing={3} sx={{ width: '100%' }}>
-                        <TextField fullWidth label="Nome da empresa" variant="outlined" />
-                        <TextField fullWidth label="Nome" variant="outlined" />
-                        <TextField fullWidth label="Sobrenome" variant="outlined" />
-                        <TextField fullWidth label="CNPJ" variant="outlined" />
-                        <TextField fullWidth label="Número de contato" variant="outlined" />
-                        <TextField fullWidth label="E-mail" variant="outlined" />
-                        <TextField fullWidth label="Crie uma senha forte" variant="outlined" />
-                        <Button variant='contained' size='large'>Cadastrar</Button>
+                        <TextField name='empresa' value={formData.empresa} onChange={handleChange} fullWidth label="Nome da empresa" variant="outlined"
+                        error={errors.empresa} helperText={errors.empresa ? 'Campo obrigatório' : ''}  />
+                        <TextField name='nome' value={formData.nome} onChange={handleChange} fullWidth label="Nome" variant="outlined" 
+                        error={errors.nome} helperText={errors.empresa ? 'Campo obrigatório' : ''} /> 
+                        <TextField name='sobrenome' value={formData.sobrenome} onChange={handleChange} fullWidth label="Sobrenome" variant="outlined" 
+                        error={errors.sobrenome} helperText={errors.empresa ? 'Campo obrigatório' : ''} />
+                        <TextField name='cnpj' value={formData.cnpj} onChange={handleChange} fullWidth label="CNPJ" variant="outlined" 
+                        error={errors.cnpj} helperText={errors.empresa ? 'Campo obrigatório' : ''} />
+                        <TextField name='contato' value={formData.contato} onChange={handleChange} fullWidth label="Número de contato" variant="outlined" 
+                        error={errors.contato} helperText={errors.empresa ? 'Campo obrigatório' : ''} />
+                        <TextField name='email' value={formData.email} onChange={handleChange} fullWidth label="E-mail" variant="outlined" 
+                        error={errors.email} helperText={errors.empresa ? 'Campo obrigatório' : ''} />
+                        <TextField name='senha' value={formData.senha} onChange={handleChange} fullWidth label="Crie uma senha forte" variant="outlined" 
+                        error={errors.senha} helperText={errors.empresa ? 'Campo obrigatório' : ''} />
+                        <Button variant='contained' size='large' onClick={handleSubmit}>Cadastrar</Button>
                     </Stack>
                 </Stack>
             </Box>
--- src/telas/Login/Login.jsx
+++ src/telas/Login/Login.jsx
@@ -2,10 +2,21 @@ import './Login.css';
 import iconBox from '/images/icon-caixa.svg';
 import { useNavigate } from 'react-router';
 import LogoProjeto from '../../components/LogoProjeto/LogoProjeto'
+import { useState } from 'react';
 
 
 function TelaLogin() {
     const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        usuarioEmail: '',
+        senha: ''
+    });
+
+    const [errors, setErrors] = useState({
+        usuarioEmail: false,
+        senha: false
+    });
 
     const handleCadastroClick = (e) => {
         e.preventDefault();
@@ -14,9 +25,36 @@ function TelaLogin() {
 
     const handleEntrarClick = (e) => {
         e.preventDefault();
-        setUsuarioLogado(true);
-        navigate('/')
+        // setUsuarioLogado(true);
+        // navigate('/')
+
+         // Validação simples dos campos
+        const novosErros = {
+            usuarioEmail: formData.usuarioEmail.trim() === '',
+            senha: formData.senha.trim() === ''
+        };
+
+        setErrors(novosErros);
+
+        // Se tiver erro, não continua
+        if(novosErros.usuarioEmail || novosErros.senha){
+            return;
+        }
+
+        // Se passou na validação
+        // Aqui você pode colocar a lógica real de login
+        alert('Login realizado com sucesso!');
+        navigate('/');
     }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prev => ({ ...prev, [name]: value }));
+
+        if (value.trim() !== '') {
+            setErrors(prev => ({ ...prev, [name]: false }));
+        }
+    };
 
     return (
         <div className="login-container">
@@ -28,18 +66,26 @@ function TelaLogin() {
                 <label className="login-label">Email ou nome de Usuário</label>
                 <input
                     type="text"
-                    className="login-input"
+                    name='usuarioEmail'
+                    className={`login-input ${errors.usuarioEmail ? 'input-error': ''}`}
                     placeholder="Insira o nome do seu usuário ou E-mail"
+                    value={formData.usuarioEmail}
+                    onChange={handleChange}
                 />
+                {errors.usuarioEmail && <span className='error-text'>Campo obrigatório</span>}
 
                 <label className="login-label">Senha</label>
                 <div className="password-wrapper">
                     <input
                         type="password"
-                        className="login-input"
+                        name='senha'
+                        className={`login-input ${errors.senha ? 'input-error' : ''}`}
                         placeholder="Insira sua senha"
+                        value={formData.senha}
+                        onChange={handleChange}
                     />
                 </div>
+                {errors.senha && <span className='error-text'>Campo obrigatório</span>}
 
                 <a className="login-link">Esqueceu sua senha?</a>
 
