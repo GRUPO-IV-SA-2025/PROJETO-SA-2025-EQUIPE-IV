9150e8945d95c40e96d503d45b161a994251967e
IAN FERREIRA DA SILVA <ian_f_silva@estudante.sesisenai.org.br>
2025-06-12 20:16:31 -0300

    Alteração do gráfico Pagina Inicial

--- src/pages/PaginaInicial/PaginaInicial.jsx
+++ src/pages/PaginaInicial/PaginaInicial.jsx
@@ -2,6 +2,12 @@ import { Box, Grid, Stack, Button, Typography, Paper, TextField } from "@mui/mat
 import Header from '../../components/Header/Header';
 import './PaginaInicial.css';
 import { LineChart } from '@mui/x-charts/LineChart';
+import { ChartContainer } from '@mui/x-charts/ChartContainer';
+import { BarPlot } from '@mui/x-charts/BarChart';
+import { ChartsXAxis } from '@mui/x-charts/ChartsXAxis';
+import { ChartsYAxis } from '@mui/x-charts/ChartsYAxis';
+import { BarLabel } from '@mui/x-charts';
+
 import { PieChart } from "@mui/x-charts";
 import { useNavigate } from "react-router";
 
@@ -23,9 +29,9 @@ function PaginaInicial() {
                     <Typography variant="h4" gutterBottom sx={{ color: "#004468", fontWeight: "bold", fontSize: "40px" }}>Home</Typography>
                 </Box>
 
-                <Box sx={{display: 'flex', justifyContent: 'space-around', alignItems: 'center', }}>
-                        
-                    <Grid container spacing={4} mb={5} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <Box sx={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', }}>
+
+                    <Grid container spacing={4} mb={5} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                         <Grid>
                             <Paper elevation={4} sx={{ p: 2 }}>
                                 <Typography variant="subtitle1" fontWeight="bold" color="white" bgcolor="#004468" p={1} borderRadius="8px 8px 8px 8px">
@@ -55,7 +61,25 @@ function PaginaInicial() {
                                     Quantidade de Estoque
                                 </Typography>
                                 <Box sx={{ mt: 2, bgcolor: 'grey.100' }}>
-                                    <PieChart
+
+                                    <ChartContainer
+                                        xAxis={[{ scaleType: 'band', data: ['A', 'B', 'C'] }]}
+                                        series={[
+                                            {
+                                                type: 'bar',
+                                                id: 'base',
+                                                data: [5, 17, 11],
+                                            },
+                                        ]}
+                                        height={300}
+                                        width={500}
+                                    >
+                                        <BarPlot barLabel="value" slots={{ barLabel: BarLabel }} />
+                                        <ChartsXAxis />
+                                        <ChartsYAxis />
+                                    </ChartContainer>
+
+                                    {/* <PieChart
                                         series={[
                                             {
                                                 data: [
@@ -67,7 +91,7 @@ function PaginaInicial() {
                                         ]}
                                         height={300}
                                         width={500}
-                                    />
+                                    /> */}
                                 </Box>
                             </Paper>
                         </Grid>
